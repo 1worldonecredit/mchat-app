7e9415fa987493d1b909324ed3f5ab8b511c9068
1worldonecredit <168613138+1worldonecredit@users.noreply.github.com>
2026-09-04 13:15:22 +0700

    profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -562,14 +562,7 @@ const handleImageUpload = async (e, type) => {
               </button>
 
 
-              {/* แถบล่างสุด: ส่งคำสั่งทั้ง 2 รูปแบบกลับไปให้ App.jsx */}
-      <div className="shrink-0 w-full z-30 bg-[var(--app-bg)] border-t border-[var(--border-color)] pb-safe">
-        <BottomNav
-          activeMenu="profile" // ระบุว่ากำลังแสดงหน้าโปรไฟล์
-          setCurrentScreen={setCurrentScreen}
-          onMenuChange={onMenuChange}
-        />
-      </div>
+
             </div>
           </div>
         </div>
